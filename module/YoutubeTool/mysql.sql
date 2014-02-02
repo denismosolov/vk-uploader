1c62070dc5fcb790ee7405bf9d5290c59d556ec7
Denis Mosolov <denismosolov@gmail.com>
2014-02-02 14:01:07 +0400

    SQL for mysql table

---
+++ module/YoutubeTool/mysql.sql
@@ -0,0 +1,6 @@
+
+CREATE DATABASE IF NOT EXISTS vk_tool;
+
+CREATE TABLE IF NOT EXISTS youtube_videos (id VARCHAR(32) NOT NULL PRIMARY KEY, playlist_title VARCHAR(128), video_title VARCHAR(128), description TEXT, sitename ENUM ('russianpod101', 'arabicpod101', 'thaipod101'));
+
+ALTER TABLE youtube_videos DROP PRIMARY KEY, ADD PRIMARY KEY USING HASH (id);
